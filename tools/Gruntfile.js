1e71d9c08aabf14319e87ccf0312918d3abc7d77
R cqls <rdrouilh@gmail.com>
2014-01-13 08:16:06 +0100

    grunt-nodewebkit-builder

---
+++ tools/Gruntfile.js
@@ -0,0 +1,22 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    nodewebkit: {
+      options: {
+        build_dir: '../../build', // Where the build version of my node-webkit app is saved
+        version: '0.8.4', //0.8.4 fails do not know why???? TODO LATER
+        //credits: './public/credits.html',
+        mac_icns: '/Users/remy/Github/dynStudio/src/icon.icns', // Path to the Mac icon file
+        mac: true, // We want to build it for mac
+        win: true, // We want to build it for win
+        linux32: true, // We don't need linux32
+        linux64: true, // We don't need linux64
+      },
+      src: '/Users/remy/Github/dynStudio/src/**/*' // Your node-webkit app
+    },
+  });
+
+  grunt.loadNpmTasks('grunt-node-webkit-builder');
+  grunt.registerTask('default', ['nodewebkit']);
+
+};
