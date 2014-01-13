779dd7382ef7b499cf954b6a75dc03cf0a7d2f3e
R cqls <rdrouilh@gmail.com>
2014-01-13 08:24:01 +0100

    Tools to deploy inside nw folder

--- deploy.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-cd src
-zip -r ../dynStudio.nw package.json ace pdf.js dynStudio.html node_modules
---
+++ nw/deploy.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+cd ../src
+zip -r ../nw/dynStudio.nw package.json ace pdf.js dynStudio.html node_modules
